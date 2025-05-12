d9b02a7a0f7734fdc37f3debbf933156d88d59da
John Doe <johndoe@example.com>
2025-05-12 21:25:01 +0200

    update config-files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      'object-shorthand': 'warn',
-      '@next/next/no-img-element': 'off',
-      // Hier ggf. in ignore Attribute aufzählen, die React nicht kennt
-      'react/no-unknown-property': ['error', { ignore: [] }],
-    },
-  },
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		rules: {
+			'object-shorthand': 'warn',
+			'@next/next/no-img-element': 'off',
+			// Hier ggf. in ignore Attribute aufzählen, die React nicht kennt
+			'react/no-unknown-property': ['error', { ignore: [] }],
+		},
+	},
+	{
+		ignores: [
+			'node_modules/', // Ігнорувати node_modules
+			'.next/', // Ігнорувати папку .next
+			'lib/generated/prisma/**', // <-- Ігнорувати згенеровані файли Prisma
+			// Додайте інші патерни, які потрібно ігнорувати
+			// наприклад, 'dist/', 'build/'
+		],
+	},
 ];
 
 export default eslintConfig;
